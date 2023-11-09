b51bfe0b02dea0f0a529166fde15aef2e94c0aa2
Jason Schorr <jason@0xbanana.com>
2023-11-08 19:59:45 -0500

    not bad right now!

--- src/pages/api/wallpaper.ts
+++ src/pages/api/wallpaper.ts
@@ -1,95 +1,91 @@
 // pages/api/wallpaper.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import sharp from 'sharp';
+import sharp, { Blend } from 'sharp';
 import fetch from 'node-fetch';
 
-
 // Define dimensions for different phone models
-const phoneDimensions: { [key: string]: { width: number; height: number } } = {
+const phoneDimensions = {
   iphone_13_pro: { width: 1170, height: 2532 },
-  iphone_13: { width: 1170, height: 2532 },
-  samsung_galaxy_s21: { width: 1080, height: 2400 },
-  google_pixel_6: { width: 1080, height: 2340 },
-  oneplus_9: { width: 1080, height: 2400 },
-  samsung_galaxy_note_20: { width: 1080, height: 2400 },
-  xiaomi_mi_11: { width: 1080, height: 2400 },
-  oppo_find_x3_pro: { width: 1440, height: 3216 },
-  sony_xperia_1_iii: { width: 1644, height: 3840 },
+  // ... other phone models
   solana_saga: { width: 1080, height: 2400 }, // Replace with actual dimensions
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { model, collection, id } = req.query;
-
-  if (typeof collection !== 'string' || typeof id !== 'string' || !collection || !id) {
-    res.status(400).json({ error: 'Invalid or missing collection or NFT ID' });
-    return;
+async function fetchNFTImage(collection: string, id: string): Promise<Buffer> {
+  const nftResponse = await fetch(`http://localhost:3000/api/nft?collection=${collection}&id=${id}`);
+  if (!nftResponse.ok) {
+    throw new Error(`NFT API responded with status: ${nftResponse.status}`);
   }
-
-  const dimensions = phoneDimensions[model as string];
-  if (!dimensions) {
-    res.status(400).json({ error: 'Invalid phone model' });
-    return;
+  const { imageUrl } = await nftResponse.json();
+  const imageResponse = await fetch(imageUrl);
+  if (!imageResponse.ok) {
+    throw new Error(`Image fetch responded with status: ${imageResponse.status}`);
   }
+  return Buffer.from(await imageResponse.arrayBuffer());
+}
 
-  try {
-    // Call the /api/nft endpoint to get the NFT image
-    const nftResponse = await fetch(`http://localhost:3000/api/nft?collection=${collection}&id=${id}`);
-    if (!nftResponse.ok) {
-      throw new Error(`NFT API responded with status: ${nftResponse.status}`);
-    }
-
-    let nftImageUrl = await nftResponse.json();
-    //@ts-ignore
-    nftImageUrl = nftImageUrl.imageUrl
-
-    const imageResponse = await fetch(nftImageUrl)
-    if (!nftResponse.ok) {
-      throw new Error(`NFT API responded with status: ${nftResponse.status}`);
-    }
-    const imageResponseBuffer = await imageResponse.buffer();
+async function getTileImage(id: string): Promise<Buffer> {
+  const jsonUrl = `https://madlads.s3.us-west-2.amazonaws.com/json/${id}.json`;
+  const response = await fetch(jsonUrl);
+  const json = await response.json();
+  const backgroundAttribute = json.attributes.find(attr => attr.trait_type === "Background");
+  return sharp(`src/bg/${backgroundAttribute.value}-bkg.png`).toBuffer();
+}
 
 
-    // Sample the color at position (0,0)
-    const color = await sharp(imageResponseBuffer).extract({ left: 0, top: 0, width: 1, height: 1 }).raw().toBuffer();
-    const [red, green, blue] = color;
+async function createWallpaper(imageBuffer: Buffer, dimensions, backgroundImageBuffer: Buffer): Promise<Buffer> {
+  // Resize the background image to cover the dimensions of the wallpaper
+  const resizedBackground = await sharp(backgroundImageBuffer)
+    .resize(dimensions.width, dimensions.height, {
+      fit: 'cover'
+    })
+    .toBuffer();
 
-    // Create a new image with the sampled color as the background
-    const wallpaper = sharp({
-      create: {
+  // Resize the NFT image if it's larger than the background
+  const originalImageMetadata = await sharp(imageBuffer).metadata();
+  let resizedNftImageBuffer = imageBuffer;
+  if (originalImageMetadata.width > dimensions.width || originalImageMetadata.height > dimensions.height) {
+    resizedNftImageBuffer = await sharp(imageBuffer)
+      .resize({
         width: dimensions.width,
         height: dimensions.height,
-        channels: 4,
-        background: { r: red, g: green, b: blue, alpha: 1 },
-      },
-    }).png();
+        fit: 'inside', // This ensures the image is resized proportionally
+        withoutEnlargement: true // This ensures the image is not enlarged
+      })
+      .toBuffer();
+  }
 
-    // Get metadata of the NFT image
-    const originalImage = await sharp(imageResponseBuffer).metadata();
+  // Calculate position to center the resized NFT image on the background
+  const resizedImageMetadata = await sharp(resizedNftImageBuffer).metadata();
+  const left = (dimensions.width - resizedImageMetadata.width) / 2;
+  const top = (dimensions.height - resizedImageMetadata.height) / 2;
 
-    // Resize the NFT image if it's larger than the background
-    let resizedNftImageBuffer = imageResponseBuffer;
-    if (originalImage.width! > dimensions.width || originalImage.height! > dimensions.height) {
-      resizedNftImageBuffer = await sharp(imageResponseBuffer)
-        .resize({
-          width: dimensions.width,
-          height: dimensions.height,
-          fit: 'inside', // This ensures the image is resized proportionally
-        })
-        .toBuffer();
+  // Composite the resized NFT image over the resized background
+  const finalImageBuffer = await sharp(resizedBackground)
+    .composite([{ input: resizedNftImageBuffer, left: Math.round(left), top: Math.round(dimensions.height - resizedImageMetadata.height) }])
+    .toBuffer();
+
+  return finalImageBuffer;
+}
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { model, collection, id } = req.query;
+    if (typeof collection !== 'string' || typeof id !== 'string') {
+      return res.status(400).json({ error: 'Invalid or missing collection or NFT ID' });
+    }
+    const dimensions = phoneDimensions[model as keyof typeof phoneDimensions];
+    if (!dimensions) {
+      return res.status(400).json({ error: 'Invalid phone model' });
     }
 
-    // Calculate position to center the resized image
-    const resizedImageMetadata = await sharp(resizedNftImageBuffer).metadata();
-    const left = (dimensions.width - resizedImageMetadata.width!) / 2;
-    const top = (dimensions.height - resizedImageMetadata.height!) / 2;
+    const imageBuffer = await fetchNFTImage(collection, id);
+    let tileImageBuffer;
+    if (collection === "Mad Lads") {
+      tileImageBuffer = await getTileImage(id);
+    }
 
-    // Composite the resized NFT image over the background
-    const finalImage = await wallpaper
-      .composite([{ input: resizedNftImageBuffer, left: Math.round(left), top: Math.round(top) }])
-      .toBuffer();
-
-    // Send the final image to the user
+    const finalImage = await createWallpaper(imageBuffer, dimensions, tileImageBuffer);
     res.setHeader('Content-Type', 'image/png');
     res.send(finalImage);
   } catch (error) {
